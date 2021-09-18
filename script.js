e7be84728dbf634b962ae023da02645673219fe6
mohsen.rasti <mohsen.rasti@yahoo.com>
2021-09-18 22:41:41 +0430

    Your new repository will be created as Todo-List.

---
+++ script.js
@@ -0,0 +1,29 @@
+const form=document.getElementById('form')
+const input=document.getElementById('input')
+
+const todosUL=document.getElementById('todos')
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault()
+
+    addTodo()
+})
+
+function addTodo(todo){
+    let todoText=input.value
+    if(todo){
+        todoText=todo.text
+    }
+    if(todoText){
+        const todoEl=document.createElement('li')
+        if(todo && todo.completed){
+            todoEl.classList.add('completed')
+        }
+        todoEl.innerText=todoText
+        
+        todo.addEventListener('click', () => todoEl.classList.toggle('completed'))
+        
+        todosUL.appendChild(todoEl)
+        input.value=''
+    }
+}
